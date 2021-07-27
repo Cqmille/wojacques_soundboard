8c5863caed6d7a037e81db2a2d862e626b99d24b
Cqmille <70196927+Cqmille@users.noreply.github.com>
2021-07-27 03:13:17 +0200

    design update

---
+++ test.js
@@ -0,0 +1,8 @@
+console.log('hi');
+
+VAR_AVAILABLE = ("123456789azertyuiop");
+//i = VAR_AVAILABLE.charAt(math.random);
+//j = Math.random(VAR_AVAILABLE.length);
+
+console.log('string chars : ' + VAR_AVAILABLE.length);
+console.log('mathrandom : '+ Math.floor(Math.random()*VAR_AVAILABLE.length));
